60338fc04602d0dce7ab3e486062a2d25ecc6b25
Mohamed ElSanteel <89236937+yonko-mo@users.noreply.github.com>
2026-02-19 01:25:50 +0200

    0 ms (100%) 151.9 MB (36.13%)

--- LeetCode/Easy/0136-single-number/0136-single-number.dart
+++ LeetCode/Easy/0136-single-number/0136-single-number.dart
@@ -1,16 +1,10 @@
 class Solution {
   int singleNumber(List<int> nums) {
-   Map<int, int> count = {};
-  for (var element in nums) {
-    count[element] = (count[element] ?? 0) + 1;
+    int result = 0;
+
+  for (var n in nums) {
+    result ^= n;
   }
-  int result = 0;
-  for (var element in count.entries) {
-    if (element.value == 1) {
-      result = element.key;
-      break;
-    }
-  }
-  return result; 
+  return result;
   }
 }
